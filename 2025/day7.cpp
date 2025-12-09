908e71ff25afe836012ea6a61af07b0ec0d8a583
Otsute <rahatpracharktam@gmail.com>
2025-12-09 16:18:49 +0700

    Day 7 part 2

--- 2025/day7.cpp
+++ 2025/day7.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <vector>
 
+using ll = long long;
+
 void part_one() {
     std::ifstream f("./puzzle_input/day7.txt");
     std::vector<std::string> grid;
@@ -28,7 +30,40 @@ void part_one() {
     std::cout << count << '\n';
 }
 
+// Used memoization here!!! It insanely increased speed
+ll count_timeline(std::vector<std::string> &grid, std::vector<std::vector<ll>> &memo, size_t level, size_t beam) {
+    if (level == grid.size() - 1) {
+        return 1;
+    }
+    if (memo[level][beam] != -1) {
+        return memo[level][beam];
+    }
+    ll count = 0;
+    if (grid[level][beam] == '^') {
+        count += count_timeline(grid, memo, level + 1, beam - 1);
+        count += count_timeline(grid, memo, level + 1, beam + 1);
+    } else {
+        count += count_timeline(grid, memo, level + 1, beam);
+    }
+
+    memo[level][beam] = count;
+    return count;
+}
+
+void part_two() {
+    std::ifstream f("./puzzle_input/day7.txt");
+    std::vector<std::string> grid;
+    for (std::string line; getline(f, line);) {
+        grid.push_back(line);
+    }
+    int beam = grid[0].find_first_of('S');
+    std::vector<std::vector<ll>> memo(grid.size(), std::vector<ll>(grid[0].size(), -1));
+    std::cout <<  count_timeline(grid, memo, 1, beam) << '\n';
+    
+}
+
 int main() {
     part_one();
+    part_two();
     return 0;
 }
