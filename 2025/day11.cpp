8e7b62a9e1ebc67cac4ba3f4deca1f18caff687c
Otsute <rahatpracharktam@gmail.com>
2026-05-03 13:53:32 +1000

    Day 11 Part 2

--- 2025/day11.cpp
+++ 2025/day11.cpp
@@ -1,19 +1,14 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
+#include <bits/stdc++.h>
 #include <unordered_map>
-#include <sstream>
-#include <stack>
-#include <stack>
-
 using namespace std;
 
 typedef unordered_map<string, vector<string>> Graph;
+typedef long long ll;
 
-Graph parse_graph() {
+Graph parse_graph(string path) {
     Graph g;
-    ifstream f("./puzzle_input/day11.txt");
-    // ifstream f("./puzzle_input/test.txt");
+    // ifstream f("./puzzle_input/day11.txt");
+    ifstream f(path);
     for (string l; getline(f, l);) {
         int i = l.find(':');
         string u = l.substr(0, i);
@@ -39,26 +34,90 @@ void print_graph(Graph &g) {
     }
 }
 
-void part_one() {
-    // Perform DFS that counts total number of paths to reach out
-    Graph g = parse_graph();
-    int count = 0;
-    stack<string> s;
-    s.push("you");
-    while (!s.empty()) {
-        string u = s.top();
-        s.pop();
-        if (u == "out") {
-            count += 1;
-        } else {
-            for (auto n : g[u]) {
-                s.push(n);
+ll count_path(Graph g, string in, string out) {
+    unordered_map<string, ll> count;
+    unordered_map<string, bool> visited;
+    for (auto p : g) {
+        count.insert({p.first, 0});
+        visited.insert({p.first, false});
+    }
+    count[in] = 1;
+    visited[in] = true;
+
+    queue<string> q;
+    q.push(in);
+    while (!q.empty()) {
+        string u = q.front();
+        q.pop();
+        for (auto v : g[u]) {
+            count[v] += count[u];
+            if (!visited[v]) {
+                q.push(v);
             }
+            visited[v] = true;
         }
     }
-    std::cout << count << '\n';
+    return count[out];
 }
 
+void part_one() {
+    // Perform BFS that counts total number of paths to reach out
+    Graph g = parse_graph("./puzzle_input/day11.txt");
+    std::cout << count_path(g, "you", "out") << '\n';
+}
+
+void print_path(vector<string> path) {
+    for (auto e : path) {
+        std::cout << e << " ";
+    }
+    std::cout << "\n";
+}
+
+// Holy Maccaroni optimization right here
+// Credit: vss2sn (https://github.com/vss2sn/advent_of_code/blob/master/2025/cpp/day_11b.cpp)
+// I basically have the same algorithm as him
+unordered_map<string, ll> history;
+
+ll recursive_count_path(Graph g, string curr, bool found_dac, bool found_fft, vector<string> path) {
+    path.push_back(curr);
+    // print_path(path);
+    
+    // Base case
+    if (curr == "out") {
+        if (found_dac && found_fft) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    string key = curr + (found_dac ? "1" : "0") + (found_fft ? "1" : "0");
+    if (history.count(key) > 0) {
+        return history[key];
+    }
+
+    ll count = 0;
+    for (auto v : g[curr]) {
+        count += recursive_count_path(g, v, found_dac || v == "dac", found_fft || v == "fft", path);
+    }
+
+    history[key] = count;
+
+    path.pop_back();
+    return count;
+}
+
+void part_two() {
+    // Count number of paths that pass through fft and dac with DFS
+    Graph g = parse_graph("./puzzle_input/day11.txt");
+    vector<string> path;
+    ll path_count = recursive_count_path(g, "svr", false, false, path);
+    cout << path_count << '\n';
+}
+
+
+
 int main() {
-    part_one();
+    // part_one();
+    part_two();
 }
