82b78778e53c2594522941219edc2ac3d2d58490
Otsute <rahatpracharktam@gmail.com>
2026-05-11 21:42:36 +1000

    Finished 2024 day 13 part 2

--- 2024/src/bin/day13.rs
+++ 2024/src/bin/day13.rs
@@ -5,12 +5,12 @@ use itertools::Itertools;
 
 #[derive(Debug)]
 struct Machine {
-    button_a: (usize, usize),
-    button_b: (usize, usize),
-    prize: (usize, usize),
+    a: (i128, i128),
+    b: (i128, i128),
+    prize: (i128, i128),
 }
 
-fn parse_x_y(mut line: &str) -> (usize, usize) {
+fn parse_x_y(mut line: &str) -> (i128, i128) {
     let mut start = line.find("+").or_else(|| line.find("=")).unwrap() + 1;
     let end = line.find(",").unwrap();
     let x = line[start..end].parse().unwrap();
@@ -28,21 +28,21 @@ fn parse_machines(path: &str) -> Vec<Machine> {
         .chunks(3)
         .into_iter()
         .map(|mut chunk| Machine {
-            button_a: parse_x_y(chunk.next().unwrap()),
-            button_b: parse_x_y(chunk.next().unwrap()),
+            a: parse_x_y(chunk.next().unwrap()),
+            b: parse_x_y(chunk.next().unwrap()),
             prize: parse_x_y(chunk.next().unwrap()),
         })
         .collect()
 }
 
-fn calc_fewest_tokens(machine: &Machine) -> Option<usize> {
+fn calc_fewest_tokens(machine: &Machine) -> Option<i128> {
     // Using the hint that each button is pressed at most 100 times,
     // we bruteforce for the prize locations
     let mut tokens = None;
     for a_presses in 0..101 {
         for b_presses in 0..101 {
-            let x = a_presses * machine.button_a.0 + b_presses * machine.button_b.0;
-            let y = a_presses * machine.button_a.1 + b_presses * machine.button_b.1;
+            let x = a_presses * machine.a.0 + b_presses * machine.b.0;
+            let y = a_presses * machine.a.1 + b_presses * machine.b.1;
             if x == machine.prize.0 && y == machine.prize.1 {
                 let cost = 3 * a_presses + b_presses;
                 tokens = tokens.map_or(Some(cost), |t| Some(cmp::min(cost, t)));
@@ -52,9 +52,37 @@ fn calc_fewest_tokens(machine: &Machine) -> Option<usize> {
     tokens
 }
 
+// Solve two linear equations with 2 unknowns
+fn calc_fewest_tokens_part_2(machine: &Machine) -> Option<i128> {
+    let prize = (
+        machine.prize.0 + 10000000000000,
+        machine.prize.1 + 10000000000000,
+    );
+    let (ax, ay, bx, by, px, py) = (
+        machine.a.0,
+        machine.a.1,
+        machine.b.0,
+        machine.b.1,
+        prize.0,
+        prize.1,
+    );
+
+    let px_by_py_bx = px * by - py * bx;
+    let ax_by_ay_bx = ax * by - ay * bx;
+    if px_by_py_bx % ax_by_ay_bx != 0 {
+        return None;
+    }
+    let a = px_by_py_bx / ax_by_ay_bx;
+    if (px - a * ax) % bx != 0 {
+        return None;
+    }
+    let b = (px - a * ax) / bx;
+    Some(3 * a + b)
+}
+
 fn part_one() {
     let machines = parse_machines("input/day13");
-    let total: usize = machines
+    let total: i128 = machines
         .iter()
         .map(|machine| calc_fewest_tokens(machine))
         .filter_map(|tokens| tokens)
@@ -62,6 +90,17 @@ fn part_one() {
     println!("{}", total);
 }
 
+fn part_two() {
+    let machines = parse_machines("input/day13");
+    let total: i128 = machines
+        .iter()
+        .map(|machine| calc_fewest_tokens_part_2(machine))
+        .filter_map(|tokens| tokens)
+        .sum();
+    println!("{}", total);
+}
+
 fn main() {
-    part_one();
+    // part_one();
+    part_two();
 }
